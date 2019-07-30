bee2c6b9a2d49a8b2a9a01e1da795c9ea6c04fd6
Matthew <matthew.sutton09@gmail.com>
2019-07-30 14:15:13 -0400

    new pics

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Write your JS in here
 pics = [
-	"imgs/kitty_bed.jpg",
-	"imgs/kitty_basket.jpg", 
-	"imgs/kitty_laptop.jpg",
-	"imgs/kitty_door.jpg",
-	"imgs/kitty_sink.jpg",
-	"imgs/kitty_wall.jpg"
+	"imgs/Casino.PNG",
+	"imgs/FullDB.PNG", 
+	"imgs/GameOfLife.PNG",
+	"imgs/JumpingJim.PNG",
+	"imgs/Launcher.PNG",
+	"imgs/RGBHerring.PNG",	
+	"imgs/SmallDB.PNG",
+	"imgs/TripTracker.PNG"
 ]
 
 var btn = document.querySelector('#next');

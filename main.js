a5eba6f46bdde6aabab01b5b698df37c9023d9c9
Matthew <matthew.sutton09@gmail.com>
2019-07-30 23:33:31 -0400

    added project pages

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Write your JS in here
 pics = [
-	"imgs/Casino.PNG",
 	"imgs/FullDB.PNG", 
 	"imgs/GameOfLife.PNG",
 	"imgs/JumpingJim.PNG",
@@ -8,28 +7,47 @@ pics = [
 	"imgs/RGBHerring.PNG",	
 	"imgs/SmallDB.PNG",
 	"imgs/TripTracker.jpg",
+	"imgs/Casino.PNG",
 ]
 alts = [
-	"A Casino game suite with BlackJack, Roulette, and Poker. Written in C# with WPF.",
 	"A full database project with frontend HTML and backend written in Java. Database stored in Postgres and SQL.", 
 	"A cellular automata project written in Java using Processing. Allows the user to control the parameters of Conways Game of Life in an interactive and visually pleasing environment.",
 	"A graphsearch project. Uses Depth and Breadth first searches to solve the puzzle in the Jumping Jim problem.",
 	"A unified video game launcher. Allows the user to launch games from the seven major game launchers. Uses APIs from all seven launchers to allow easy access and statistics tracking for games.",
 	"An image stegonagrapy project written in MIPS using MARS. Includes six diffentent encoding methods for hiding plain text messages in bitmap images and their respective decoding methods.",	
 	"An In-Memory database project used for query practice. Uses publicly available data from basketballconference.com",
-	"An Android application written in Java. Provides the user with full control of a Google Map, allows for private and public waypoint creation, allows the user to track trips including the start, endtimes, speed and distance of the trip, as well as provides the current local weather conditions.",
+	"An Android application written in Java. Provides the user with full control of a Google Map, private and public waypoint creation, trip tracking including the start and endtimes, speed and distance of the trip, and shows the current local weather conditions.",
+	"A Casino game suite with BlackJack, Roulette, and Poker. Written in C# with WPF.",
 ]
 
+urls = [
+	"ProjectPages/fullDB.html",
+	"ProjectPages/gameOfLife.html",
+	"ProjectPages/jumpingJim.html",
+	"ProjectPages/launcher.html",
+	"ProjectPages/rgbHerring.html",
+	"ProjectPages/smallDB.html",
+	"ProjectPages/tripTracker.html",
+	"ProjectPages/casino.html",
+]
 var btn = document.querySelector('#next');
 var img = document.querySelector("img");
 var desc = document.querySelector("#desc");
-var itr = 1;
+var itr = 0;
 
 btn.addEventListener("click", function(){
 	img.src = pics[itr];
 	img.alt = alts[itr];
 	desc.textContent = alts[itr];
 	itr = (itr+1) % pics.length;
+});
+
+img.addEventListener("click", function(){
+	// save the value of the itr before moving to next page, eventually
+	if(itr>0)
+		location.href = urls[itr-1];
+	else
+		location.href = urls[urls.length-1];
 });
 
 function myFunction(x){

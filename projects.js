06fb04442f1b491208564f4603f8bb3cd81f5456
Matthew <matthew.sutton09@gmail.com>
2019-08-01 01:07:33 -0400

    nav bar with new pages

--- projects.js
+++ projects.js
@@ -1,34 +1,34 @@
 // Write your JS in here
 pics = [
-	"imgs/FullDB.PNG", 
-	"imgs/GameOfLife.PNG",
-	"imgs/JumpingJim.PNG",
-	"imgs/Launcher.PNG",
-	"imgs/RGBHerring.PNG",	
-	"imgs/SmallDB.PNG",
-	"imgs/TripTracker.jpg",
-	"imgs/Casino.PNG",
+	"../imgs/FullDB.PNG", 
+	"../imgs/GameOfLife.PNG",
+	"../imgs/JumpingJim.PNG",
+	"../imgs/Launcher.PNG",
+	"../imgs/RGBHerring.PNG",	
+	"../imgs/SmallDB.PNG",
+	"../imgs/TripTracker.jpg",
+	"../imgs/Casino.PNG",
 ]
 alts = [
 	"A full database project with frontend HTML and backend written in Java. Database stored in Postgres and SQL.", 
 	"A cellular automata project written in Java using Processing. Allows the user to control the parameters of Conways Game of Life in an interactive and visually pleasing environment.",
-	"A graphsearch project. Uses Depth and Breadth first searches to solve the puzzle in the Jumping Jim problem.",
+	"A graph search project. Uses Depth and Breadth first searches to solve the puzzle in the Jumping Jim problem.",
 	"A unified video game launcher. Allows the user to launch games from the seven major game launchers. Uses APIs from all seven launchers to allow easy access and statistics tracking for games.",
 	"An image stegonagrapy project written in MIPS using MARS. Includes six diffentent encoding methods for hiding plain text messages in bitmap images and their respective decoding methods.",	
 	"An In-Memory database project used for query practice. Uses publicly available data from basketballconference.com",
 	"An Android application written in Java. Provides the user with full control of a Google Map, private and public waypoint creation, trip tracking including the start and endtimes, speed and distance of the trip, and shows the current local weather conditions.",
-	"A Casino game suite with BlackJack, Roulette, and Poker. Written in C# with WPF.",
+	"A Casino game suite with Roulette, Blackjack, and Poker. Written in C# with WPF.",
 ]
 
 urls = [
-	"ProjectPages/fullDB.html",
-	"ProjectPages/gameOfLife.html",
-	"ProjectPages/jumpingJim.html",
-	"ProjectPages/launcher.html",
-	"ProjectPages/rgbHerring.html",
-	"ProjectPages/smallDB.html",
-	"ProjectPages/tripTracker.html",
-	"ProjectPages/casino.html",
+	"../ProjectPages/fullDB.html",
+	"../ProjectPages/gameOfLife.html",
+	"../ProjectPages/jumpingJim.html",
+	"../ProjectPages/launcher.html",
+	"../ProjectPages/rgbHerring.html",
+	"../ProjectPages/smallDB.html",
+	"../ProjectPages/tripTracker.html",
+	"../ProjectPages/casino.html",
 ]
 var btn = document.querySelector('#next');
 var img = document.querySelector("img");
@@ -50,6 +50,6 @@ img.addEventListener("click", function(){
 		location.href = urls[urls.length-1];
 });
 
-function myFunction(x){
-	x.classList.toggle("change");
-}
+// function myFunction(x){
+// 	x.classList.toggle("change");
+// }

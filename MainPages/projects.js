d354102c7bd61dcfb894189f88f6075af93b9fbc
Matthew <matthew.sutton09@gmail.com>
2019-08-15 17:42:57 -0400

    bottomText changes

--- MainPages/projects.js
+++ MainPages/projects.js
@@ -10,11 +10,11 @@ pics = [
 	"../imgs/MainImages/Casino.PNG",
 ]
 alts = [
-	"A full database project with frontend HTML and backend written in Java. \nConsisting of 9 total tables, the database models a library system. \nMembers are able to view books based on many criteria and are shown which books are available based on the criteria chosen. \nMembers are also able to see a list of their past and active transactions including times and locations that books were checked out. \nLibrarians are able to check in and out books at their home location. \nBoth member and librarian username and passwords are authenticated against two tables holding login information. \nDatabase stored in Postgres and SQL.", 
+	"A full database project with frontend HTML and backend written in Java. \n\nConsisting of 9 total tables, the database models a library system. \n\nMembers are able to view books based on many criteria and are shown which books are available based on the criteria chosen. \n\nMembers are also able to see a list of their past and active transactions including times and locations that books were checked out. \n\nLibrarians are able to check in and out books at their home location. \n\nBoth member and librarian username and passwords are authenticated against two tables holding login information. \n\nDatabase stored in Postgres and SQL.", 
 	"A cellular automata project written in Java using Processing. Allows the user to control the parameters of Conways Game of Life in an interactive and visually pleasing environment.",
 	"A graph search project. Uses Depth and Breadth first searches to solve the puzzle in the Jumping Jim problem.",
 	"A unified video game launcher. Allows the user to launch games from the seven major game launchers. Uses APIs from all seven launchers to allow easy access and statistics tracking for games.",
-	"An image stegonagrapy project written in MIPS using MARS. Includes six diffentent encoding methods for hiding plain text messages in bitmap images and their respective decoding methods.",	
+	"An image stegonagrapy project written in MIPS using MARS. Includes four diffentent encoding methods for hiding plain text messages in bitmap images and their respective decoding methods.",	
 	"An In-Memory database project used for query practice. Uses publicly available data from basketballconference.com",
 	"An Android application written in Java. Provides the user with full control of a Google Map, private and public waypoint creation, trip tracking including the start and endtimes, speed and distance of the trip, and shows the current local weather conditions.",
 	"A Casino game suite with Roulette, Blackjack, and Poker. Written in C# with WPF.",
